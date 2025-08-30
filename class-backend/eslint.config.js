64f5c54b327a53493205dd9d8db4e729f4fdab2e
zzkhong <khongc7@gmail.com>
2025-08-30 12:34:04 +0800

    chore: add express boilerplate

---
+++ class-backend/eslint.config.js
@@ -0,0 +1,31 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import perfectionist from "eslint-plugin-perfectionist";
+
+export default tseslint.config(
+  {
+    ignores: ["**/*.js"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  perfectionist.configs["recommended-natural"],
+  {
+    files: ["**/*.test.ts", "**/*.spec.ts"],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+      "@typescript-eslint/unbound-method": "off",
+    },
+  },
+);
