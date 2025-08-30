36e90b481ea0d64565c7a7d47105b0760a23d171
zzkhong <khongc7@gmail.com>
2025-08-30 18:05:19 +0800

    chore: update model org and add docker-compose for local db setup

--- class-backend/src/config/database.ts
+++ class-backend/src/config/database.ts
@@ -2,4 +2,9 @@ import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize({
   dialect: 'postgres',
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  logging: false,
 });
--- class-backend/src/models/teacher.model.ts
+++ class-backend/src/models/teacher.model.ts
@@ -1,6 +1,6 @@
 import { DataTypes, Model, Optional } from 'sequelize';
 
-import { sequelize } from '../../config/database';
+import { sequelize } from '../config/database';
 
 interface TeacherAttributes {
   id: number;
