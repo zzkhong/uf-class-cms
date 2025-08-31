efa6122f238959f7fc1f1137e9eb6976f0bc5495
zzkhong <khongc7@gmail.com>
2025-09-01 01:23:02 +0800

    chore: add postman collections and update vitest config

--- class-backend/vitest.config.js
+++ class-backend/vitest.config.js
@@ -1,3 +1,18 @@
-import { defineConfig } from "vitest/config";
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{ts,js}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      all: true,
+      include: ['src/**/*.ts'],
+      exclude: ['**/node_modules/**', '**/__tests__/**'],
+    },
+  },
+});
