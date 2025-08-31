60feb10b52653b9f26b5240bdf6e9961ab95d5a6
zzkhong <khongc7@gmail.com>
2025-08-31 23:04:49 +0800

    chore: update meaningful validation err message

--- class-backend/src/middlewares/validate.ts
+++ class-backend/src/middlewares/validate.ts
@@ -1,14 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
-import z, { ZodObject } from 'zod';
+import z, { ZodError, ZodObject } from 'zod';
 
 export const validate =
   (schema: ZodObject) => (req: Request, res: Response, next: NextFunction) => {
-    const result = schema.safeParse(req.body);
-
-    if (!result.success) {
-      return res.status(400).json({ error: z.treeifyError(result.error) });
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const message = error.issues
+          .map((issue) => `${issue.path.join('.')} is ${issue.message}`)
+          .join('. ');
+        res.status(400).json({ error: message });
+      } else {
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
     }
-
-    req.body = result.data;
-    next();
   };
