516bfb97e182379b5943c9ac96b4941a9bcb58bc
zzkhong <khongc7@gmail.com>
2025-09-02 01:04:16 +0800

    chore: cleanup be env org

--- class-backend/src/database/config/database.ts
+++ class-backend/src/database/config/database.ts
@@ -3,10 +3,9 @@ import { Sequelize } from 'sequelize';
 const dbConfig = require('./sequelize.config.js');
 
 const env = process.env.NODE_ENV || 'development';
-const config = dbConfig[env];
 
 export const sequelize = new Sequelize({
-  ...config,
+  ...dbConfig,
   dialect: 'postgres',
   logging: env === 'development' ? console.log : false,
 });
--- class-backend/src/database/config/sequelize.config.js
+++ class-backend/src/database/config/sequelize.config.js
@@ -1,20 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-  },
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
 };
